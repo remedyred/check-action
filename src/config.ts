e893d66b7d93ab32524e9d40888de436f47efa82
Snickbit <snickbit@users.noreply.github.com>
2023-05-16 13:33:59 -0400

    fix: skip git setup when not needed

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ import {getFileJSON} from '@snickbit/node-utilities'
 
 interface State {
 	isDebug: boolean
+	gitReady: boolean
 	input?: Input
 	secrets: Record<string, any>
 	pm?: 'npm' | 'pnpm' | 'yarn'
@@ -13,6 +14,7 @@ interface State {
 
 const state: State = {
 	isDebug: false,
+	gitReady: false,
 	secrets: {}
 }
 
@@ -28,16 +30,47 @@ export function isDebug() {
 	return state.isDebug
 }
 
-interface Input {
+export function isGitReady() {
+	return state.gitReady
+}
+
+export function setGitReady(ready = true) {
+	state.gitReady = ready
+}
+
+export enum InputKeys {
+	PACKAGE_MANAGER = 'PACKAGE_MANAGER',
+	SCRIPTS = 'SCRIPTS',
+	NO_BAIL = 'NO_BAIL',
+	BAIL_ON_MISSING = 'BAIL_ON_MISSING',
+	AUTOFIX_LOCKFILE = 'AUTOFIX_LOCKFILE',
+	AUTOFIX_LINT = 'AUTOFIX_LINT',
+	BAIL_ON_DIRTY = 'BAIL_ON_DIRTY',
+	AUTO_COMMIT = 'AUTO_COMMIT',
+	DEBUG = 'DEBUG',
+	GITHUB_TOKEN = 'GITHUB_TOKEN',
+	NPM_REGISTRY = 'NPM_REGISTRY',
+	NPM_TOKEN = 'NPM_TOKEN',
+	NPM_REGISTRY_SCOPE = 'NPM_REGISTRY_SCOPE'
+}
+
+const inputActionRequiresGit = new Set([
+	'AUTOFIX_LOCKFILE',
+	'AUTOFIX_LINT',
+	'BAIL_ON_DIRTY',
+	'AUTO_COMMIT'
+])
+
+type MappedInput = {
+	[key in InputKeys]?: boolean | string
+}
+
+interface Input extends MappedInput {
 	PACKAGE_MANAGER: 'npm' | 'pnpm' | 'yarn'
 	SCRIPTS: string
 	NO_BAIL: boolean
 	BAIL_ON_MISSING: boolean
 	AUTOFIX_LOCKFILE: boolean
-	AUTOFIX_LINT: boolean | string
-	BAIL_ON_DIRTY: boolean | string
-	AUTO_COMMIT: boolean | string
-	DEBUG?: boolean | string
 	GITHUB_TOKEN?: string
 	NPM_REGISTRY?: string
 	NPM_TOKEN?: string
@@ -117,3 +150,18 @@ export const hasScript = scriptName => {
 	return state.availableScripts?.includes(scriptName)
 }
 
+export const requiresGit = (name?: string) => {
+	if (name) {
+		return inputActionRequiresGit.has(name)
+	}
+	const input = useInput()
+	if (!input) {
+		return false
+	}
+
+	for (const [key, value] of Object.entries(input)) {
+		if (inputActionRequiresGit.has(key) && !!value) {
+			return true
+		}
+	}
+}
